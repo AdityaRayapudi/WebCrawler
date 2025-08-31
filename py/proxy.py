767523dace0fe3f5f7d2dc52a2334d4a22603b40
Aditya Rayapudi <arayapudi2006@gmail.com>
2025-08-31 11:16:48 -0400

    Updated proxy to use https

--- py/proxy.py
+++ py/proxy.py
@@ -1,12 +1,22 @@
 from mitmproxy import http
 import redis
 
+# Connect to Redis
 r = redis.Redis(host='127.0.0.1', port=6379, decode_responses=True)
 
 def request(flow : http.HTTPFlow) -> None:
     host = flow.request.pretty_host
     ip = r.get("ip:" + host)
 
-    # Update host and host header
-    flow.request.host = ip
-    flow.request.headers["Host"] = host
+    # Update host and host header if cached
+    if ip is not None:
+        flow.request.scheme = "https"
+        flow.request.host = host
+
+        # Port for https requests
+        flow.request.port = 443
+        flow.request.headers["Host"] = host
+
+        # Request ip 
+        flow.request.address = (ip, 443)
+
