d9789d79b210c039e62a2e5cfd35f2d93c022de4
Aditya Rayapudi <arayapudi2006@gmail.com>
2025-09-13 16:57:46 -0400

    Update status code | Removed Old Comments

--- py/app/app.py
+++ py/app/app.py
@@ -2,19 +2,17 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import asyncio
 
-# from dynamic_scraper import BrandPage
 from scraper_manager import ScraperManager, WebPage
 
 class URL(BaseModel):
     seed: str
     link: str
 
-# brand = BrandPage()
 scraper_manager = ScraperManager()
 
 app = FastAPI()
 
-@app.post("/parsed-pages/")
+@app.post("/parsed-pages/", status_code=201)
 async def parse_page(url: URL):
     url_dict = url.model_dump()
     seed, link = url_dict.get("seed"), url_dict.get("link")
@@ -31,12 +29,3 @@ async def parse_page(url: URL):
     await scraper_manager.close()
 
     return page_details
-    # return page_details
-
-# @app.post("/parsed-pages/")
-# async def parse_page(url: URL):
-#     url_dict = url.model_dump()
-#     ip, seed, q = [url_dict.get("ip"), url_dict.get("seed"), url_dict.get("q")]
-#     page_data = await brand.open_page(ip = ip, seed= seed, link=q)
-
-#     return page_data
